667d5d603db460f6e22ebdf4a06958218547aa64
hiego-f <hiegof@gmail.com>
2016-04-25 20:29:09 -0300

    Added files via upload

--- sala.php
+++ sala.php
@@ -5,13 +5,13 @@
 		if (!$conexao){
 			echo "Não foi possível estabelecer uma conexão com o Banco.";
 		}else{
-			$sala = $_POST ['sala'];
+			$sala = $_POST ['_sala'];
 			$comprimento = $_POST ['comprimento'];
 			$largura = $_POST ['largura'];
 			$codPredio = $_POST ['codPredio'];
 			$siglaDpto =  strip_tags( $_POST ['siglaDpto']);
 			
-			$query = "INSERT INTO sala VALUES ('$_POST[sala]','$_POST[comprimento]','$_POST[largura]', '$_POST[codPredio]' ,'$_POST[siglaDpto]')";  
+			$query = "INSERT INTO sala VALUES ('$_POST[_sala]','$_POST[comprimento]','$_POST[largura]', '$_POST[codPredio]' ,'$_POST[siglaDpto]')";  
 			pg_query($query);
 			
 			echo "Sala cadastrada com sucesso!!";
