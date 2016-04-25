07a23a4fa493bc65dbc37f577a91ea94cd727c3f
fsribas <fsribas.si@Gmail.com>
2016-04-25 20:11:19 -0300

    Added files via upload

---
+++ Cadastro_Departamento.php
@@ -0,0 +1,25 @@
+<?php
+
+$SiglaDepa=$_POST['sigla'];
+$NomeDepa=$_POST['nome'];
+$ResponsavelDepa=$_POST['responsavel'];
+
+
+
+$servidor = "host=localhost port=5432 dbname=academico user=postgres password=123456";
+$usuario = "postgres";
+$senha = "123456";
+
+
+
+$conexao = pg_connect($servidor) or die ("Não foi possivel conectar ao servidor PostGreSQL");
+//$ordenacao=$_POST['ordenacao'];
+//falta configurar as ordenações
+
+
+
+
+
+$result = pg_query($conexao, "INSERT INTO Cadastro Departamento VALUES ('".$SiglaDepa."', '".$NomeDepa."',
+	'".$ResponsavelDepa."')");
+?>
