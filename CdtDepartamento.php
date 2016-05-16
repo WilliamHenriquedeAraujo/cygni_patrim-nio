526852b64848558957af3e989f54ce98705b1b93
hiego-f <hiegof@gmail.com>
2016-05-16 19:19:32 -0300

    Add files via upload

---
+++ CdtDepartamento.php
@@ -0,0 +1,22 @@
+﻿<?php
+         $strBd = "host=localhost port=5432 dbname=cygni user=postgres password=root";
+		 $conexao = pg_connect($strBd);
+		 
+		if (!$conexao){
+			echo "Não foi possível estabelecer uma conexão com o Banco.";
+		}else{
+			$sigla = $_POST ['sigla'];
+			$nome =  strip_tags($_POST ['nome']);
+			$responsavel =  strip_tags( $_POST ['responsavel']);
+			
+			$query = "INSERT INTO departamento VALUES ('$_POST[sigla]','$_POST[nome]','$_POST[responsavel]')";  
+			pg_query($query);
+			
+			echo "Prédio cadastrado com sucesso!!";
+			
+		}
+		
+		pg_close($conexao);	
+			
+			
+?>
---
+++ sala.php
@@ -0,0 +1,25 @@
+﻿<?php
+         $strBd = "host=localhost port=5432 dbname=cygni user=postgres password=root";
+		 $conexao = pg_connect($strBd);
+		 
+		if (!$conexao){
+			echo "Não foi possível estabelecer uma conexão com o Banco.";
+		}else{
+			$_sala = $_POST ['_sala'];
+			$comprimento = $_POST ['comprimento'];
+			$largura = $_POST ['largura'];
+			$codPredio = $_POST ['codPredio'];
+			$siglaDpto =  strip_tags( $_POST ['siglaDpto']);
+			
+			$query = "INSERT INTO sala VALUES ('$_POST[_sala]','$_POST[comprimento]','$_POST[largura]', '$_POST[codPredio]' ,'$_POST[siglaDpto]')";  
+			pg_query($query);
+			
+			echo "Sala cadastrada com sucesso!!";
+			
+		}
+		
+		pg_close($conexao);	
+			
+			
+?>
+
