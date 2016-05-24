389102db8f1fa5f84b409027f08898b8c91b3166
diegoheitor <benigno99.6@gmail.com>
2016-05-23 21:01:12 -0300

    Add files via upload
    Feito

--- CdUsuario.php
+++ CdUsuario.php
@@ -1,33 +1,41 @@
 <?php
 
-	$login=$_POST["login"];
-	$nome=$_POST["nome"];
-	$senha=$_POST["senha"];
-	$nivel=$_POST["nivel"];
+$login=$_POST['login'];
 
+$nome=$_POST['nome'];
+
+$senha=$_POST['senha'];
+
+$nivel=$_POST['nivel'];
+
+
+
+
+$conexao=" host=localhost dbname=cygni port=5432 user=postgres password=123456";
+
+$status_conexao=pg_connect($conexao);
+
+
+if(!$status_conexao){
+	
+	echo "Erro na conexao com o banco de dados...";
+	
+}
+else{
 	
 	
-	$conexao=" host=localhost dbname=cygni port=5432 user=postgres password=root";
-	$status_conexao=pg_connect($conexao);
-
-	if(!$status_conexao){
-		echo "Erro na conexao com o banco de dados...";
-	}else{
-
-			$sql_insercao="INSERT INTO usuario VALUES ('".$login."', '".$nome."', '".$senha."', ".$nivel.");";
-			pg_query($status_conexao, $sql_insercao);
-			echo "<strong>A gravacao dos dados foi realizada com sucesso!<strong><br>";
-	}
-
-
-
-
-
-
-
-
-
-
-
+	$sql_insercao="INSERT INTO usuario (login,nome,senha,nivel) VALUES ('".$login."', '".$nome."', '".$senha."', '".$nivel."');";
 	
+	pg_query($status_conexao, $sql_insercao);
+	
+	echo "<strong>A gravacao dos dados foi realizada com sucesso!<strong><br>";
+	
+}
+
+
+
+
+
+
+
 ?>
