cdf9b374ae523465cfeca3491ab2455d2d1d0a20
hiego-f <hiegof@gmail.com>
2016-05-25 18:45:45 -0300

    Add files via upload

--- bem_patrimonial.php
+++ bem_patrimonial.php
@@ -1,4 +1,6 @@
-﻿<?php
+﻿
+<html>
+<?php
 
 	$numero_bem=$_POST["numero"];
 	$descricao=$_POST["descricao"];
@@ -40,16 +42,6 @@
 			echo "<strong>A gravacao dos dados foi realizada com sucesso!<strong><br>";
 	}
 
-
-
-
-
-
-
-
-
-
-
 	//echo "Dados:<br><br>
 
 	//Numero: ".$numero_bem."<br>
@@ -62,3 +54,57 @@
 	//Valor: ".$valor_bem."<br>
 	//Situacao: ".$situacao."<br>"
 ?>
+
+<body>
+	
+	
+	
+	<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+ <meta charset="utf-8">
+ <meta http-equiv="X-UA-Compatible" content="IE=edge">
+ <meta name="viewport" content="width=device-width, initial-scale=1">
+ <title>Cygni Patrimônio</title>
+
+ <link href="css/bootstrap.min.css" rel="stylesheet">
+ <link href="css/style.css" rel="stylesheet">
+</head>
+<body> 
+
+ <script src="js/jquery.min.js"></script>
+ <script src="js/bootstrap.min.js"></script>
+ 
+ 
+ <nav class="navbar navbar-inverse navbar-fixed-top">
+ <div class="container-fluid">
+  <div class="navbar-header">
+   <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+    <span class="sr-only">Toggle navigation</span>
+    <span class="icon-bar"></span>
+    <span class="icon-bar"></span>
+    <span class="icon-bar"></span>
+   </button>
+   <a class="navbar-brand" href="index.html">Cygni Patrimônio</a>
+  </div>
+  <div id="navbar" class="navbar-collapse collapse">
+   <ul class="nav navbar-nav navbar-right">
+    <li><a href="index.html">Início</a></li>
+	<li><a href="relatorio.html">Emitir Relatórios</a></li>
+     <li><a href="CdUsuario.html">Cadastrar usuário</a></li>
+	 <a href="#" class="btn btn-info btn-lg">
+          <span class="glyphicon glyphicon-log-out"></span> Log out
+        </a>
+ 
+   </ul>
+  </div>
+ </div>
+</nav>
+</br>
+</br>
+<a href="index.html" class="btn btn-default">Voltar</a>
+
+</html> 
+
+
+
